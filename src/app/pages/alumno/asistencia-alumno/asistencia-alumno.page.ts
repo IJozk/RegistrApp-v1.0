0e55050b97bcd9e18204dfc5361cb1270f22583a
jozk <jorgeriffo.u@gmail.com>
2023-11-30 17:18:32 -0300

    version casi final

--- src/app/pages/alumno/asistencia-alumno/asistencia-alumno.page.ts
+++ src/app/pages/alumno/asistencia-alumno/asistencia-alumno.page.ts
@@ -15,7 +15,10 @@ export class AsistenciaAlumnoPage implements OnInit {
   nombre: string;
   email: string;
   inscripciones: any;
-  secciones: any;
+  secciones2: any[] = [];
+  asistencias: any;
+  clases2 : any;
+  asist: any;
   
   async ngOnInit() {
 
@@ -28,27 +31,32 @@ export class AsistenciaAlumnoPage implements OnInit {
 
           this.getInscripcionesInfo('inscripcion', this.email);
           const inscripciones = localStorage.getItem('inscripcion');
-          
+
           if (inscripciones) {
             const c_secciones = JSON.parse(inscripciones);
 
             const promesas = c_secciones.map(async (objetoActual) => {
-
-              const secciones = await this.firebaseSvc.obtenerDocumentos('secciones');
-              localStorage.setItem('secciones', JSON.stringify(secciones));
-      
+              const secciones2 = await this.firebaseSvc.obtenerDocumentos('secciones');
+              localStorage.setItem('secciones2', JSON.stringify(secciones2));
+          
               // Filtrar y mapear las clases actuales
-              this.secciones = c_secciones
-                .filter(seccion => {console.log('aca estoy');
-                  console.log('ide de id_seccion: '+seccion['id_seccion']+'      id_inscripcion: '+objetoActual['id_seccion'])
-                  return seccion['id_seccion'] == objetoActual['id_seccion'];
-                }).map(seccion  => {
-                  
-                  return Object.assign({} ,seccion , {});
-                })
-            });
-            await Promise.all(promesas);
-          }else{
+              this.secciones2 = c_secciones
+                  .map(seccion => {
+                    if (seccion['id_seccion'] === objetoActual['id_seccion']) {
+                      // Agrega propiedades de objetoActual a seccion solo si cumplen con la condición
+                      return { ...seccion, ...objetoActual };
+                  } else {
+                      return seccion;
+                  }
+                  });
+          
+              console.log(this.secciones2.length);
+          });
+          
+          await Promise.all(promesas);
+          
+          }
+          else{
             null;
           };
         }else{
@@ -106,6 +114,37 @@ export class AsistenciaAlumnoPage implements OnInit {
     })
   }
 
+  async getClasesInfo(colection: string, id_seccion: string) {
+
+    const loading = await this.utilsSvc.loading();
+    await loading.present();
+
+    const opcionesConsulta = {
+      campo: "id_seccion",
+      operador: "==",
+      valor: id_seccion,
+    };
+
+
+    this.firebaseSvc.obtenerDocsWhere(colection, opcionesConsulta).then((datos) => {
+
+      localStorage.setItem(colection, JSON.stringify(datos));
+
+    }).catch(error => {
+      console.log(error)
+
+      this.utilsSvc.presentToast({
+        message: error.message,
+        duration: 2500,
+        color: 'danger',
+        position: 'middle',
+        icon: 'alert-circle-outline'
+      })
+    }).finally(() => {
+      loading.dismiss();
+    })
+  }
+
   async getSeccionesInfo(colection: string) {
 
     const loading = await this.utilsSvc.loading();
@@ -132,5 +171,55 @@ export class AsistenciaAlumnoPage implements OnInit {
   }
 
 
+  async verAsist(id: any, id_seccion: string){
 
+
+    let doc= document.getElementById(id);
+  
+
+    this.asist = JSON.parse(localStorage.getItem(this.asistencias));
+    this.getInscripcionesInfo('asistencias', this.email);
+    const inscripciones = localStorage.getItem('asistencias');
+
+    if (inscripciones) {
+    const c_secciones = JSON.parse(inscripciones);
+
+    const promesas = c_secciones.map(async (objetoActual) => {
+
+    const datos2 = await this.firebaseSvc.obtenerDocsWhere ('clases', {
+      campo: "id_seccion",
+      operador: "==",
+      valor: id_seccion,
+    });
+    localStorage.setItem('clases2', JSON.stringify(datos2));
+
+    this.clases2 = datos2
+      .map(clase2 => {
+        if (clase2['id_seccion'] === objetoActual['id_seccion'] && clase2['id_seccion'] === objetoActual['id_seccion']) {
+        const tiempoInicio = clase2['hora_ini'].seconds * 1000 + clase2['hora_ini'].nanoseconds / 1e6;
+        const fechaInicio = new Date(tiempoInicio);
+
+        const tiempoFin = clase2['hora_fin'].seconds * 1000 + clase2['hora_fin'].nanoseconds / 1e6;
+        const fechaFin = new Date(tiempoFin);
+
+        return Object.assign({}, clase2, { hora_ini: fechaInicio, hora_fin: fechaFin });
+      }       
+      else {
+        return clase2;
+    }
+      });
+    })
+    await Promise.all(promesas);
+  }
+
+    if(doc.classList.contains("ion-hide")){
+      
+    doc.classList.remove("ion-hide");
+
+    }
+    else{
+      doc.classList.add("ion-hide");
+    }
+  }
 }
+
